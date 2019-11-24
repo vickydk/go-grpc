47fea49d15222058a9a3174279bbeec0deaf157e
vicky <vicky.dwi.k@gmail.com>
2019-11-24 23:54:11 +0700

    update govendor

--- vendor/omsApi/pkg/utl/model/channel.go
+++
@@ -1,9 +0,0 @@
-package omsApi
-
-type Channel struct {
-	TableName struct{} `sql:"channel"`
-
-	Base
-	Name        string `sql:"type:varchar(100)" orm:"column(name)" json:"Name"`
-	Description string `orm:"column(description)" json:"Description"`
-}
